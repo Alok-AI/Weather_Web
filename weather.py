4ad691577375a07acb865a2440220ae73a7eb533
Alok Jain <97657043+Alok-AI@users.noreply.github.com>
2024-04-25 20:59:20 +0530

    first Commit

---
+++ weather.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+    
+def get_current_weather(city="Bhilwara") :
+
+    request_url =f'http://api.openweathermap.org/data/3.0/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    weather_data = requests.get(request_url).json()
+    return weather_data
+
+
+    if __name__ == "__main__" :
+        print('\n*** Get Current Weather Condition ***\n')
+
+        city = input("\nPlease enter a city name: ")
+        #check for empty strings with only space
+        if not bool(city.strip()) :
+            city="Kansas City"
+        weather_data = get_current_weather(city)
+        print("\n")
+        pprint(weather_data)
